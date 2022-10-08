14c7f761282e4882337fab2a8f4c74b57e7781a7
Jamir Hossain <46633915+jamircse@users.noreply.github.com>
2022-10-08 15:35:27 +0600

    Update custom.js

--- custom.js
+++ custom.js
@@ -3,38 +3,19 @@
  const months=["January","february","March","April","May","June","July","August","September","October","November","December"];
  const days=["Sunday","Monday","Tuersday","WednesDay","Thursday","Friday","Saturday"]
 
- var calender=(month)=>{
-   var current_year=date.getFullYear();
+ var calender=()=>{
    var currentdayindex=date.getDay();
-  
+   var currentdayname=days[date.getDay()];
+   var currentdate=date.getDate();
+   var current_monthindex=date.getMonth();
+   var current_month_name=months[date.getMonth()];
+   var current_year=date.getFullYear();
    let firstday_index__of_cuttent_month=new Date(current_year,current_monthindex,1).getDay();
    let lastdaycurrentmonth=new Date(current_year,current_monthindex+1,0).getDate();
    let premontlastdate=new Date(current_year,current_monthindex,0).getDate();
- }
- 
- var currentdayindex=date.getDay();
- var currentdayname=days[date.getDay()];
- var currentdate=date.getDate();
- var current_monthindex=date.getMonth();
- var current_month_name=months[date.getMonth()];
- var current_year=date.getFullYear();
- let firstday_index__of_cuttent_month=new Date(current_year,current_monthindex,1).getDay();
- let lastdaycurrentmonth=new Date(current_year,current_monthindex+1,0).getDate();
- let premontlastdate=new Date(current_year,current_monthindex,0).getDate();
-
-// next month
- var nextbtn=document.querySelector(".next");
- nextbtn.addEventListener("click",()=>{
-    current_monthindex+=1;
-    dateshedule.innerHTML="";
-    
- });
-
-
-
-
-var dateshedule=document.querySelector(".date");
- var dates=" ";
+  
+   var dateshedule=document.querySelector(".date");
+   var dates=" ";
 
  // pre month
  for(let x=firstday_index__of_cuttent_month;x>0;x--){
@@ -57,6 +38,28 @@ for (let index =1; index <=lastdaycurrentmonth; index++) {
  dateshedule.innerHTML=dates;
  dates="";
 
-document.querySelector(".current_month h2").innerHTML=current_month_name;
-document.querySelector(".current_month h4").innerHTML=`${currentdayname} ${currentdate} ${current_month_name} ${current_year}`;
+  document.querySelector(".current_month h2").innerHTML=current_month_name;
+  document.querySelector(".current_month h4").innerHTML=`${currentdayname} ${currentdate} ${current_month_name} ${current_year}`;
+ }
  
+ 
+
+// next month
+ var nextbtn=document.querySelector(".next");
+ nextbtn.addEventListener("click",()=>{
+    date.setMonth(date.getMonth()+1);
+    calendar();
+    
+ });
+// pre month
+ var prebtn=document.querySelector(".pre");
+ prebtn.addEventListener("click",()=>{
+    date.setMonth(date.getMonth()-1);
+    calendar();
+    
+ });
+
+calendar();
+
+
+
