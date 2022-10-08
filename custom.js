ca1fe23c23df5978506c983f1078507aeb53275d
Jamir Hossain <46633915+jamircse@users.noreply.github.com>
2022-10-08 12:05:31 +0600

    Update custom.js

--- custom.js
+++ custom.js
@@ -3,22 +3,22 @@
  const months=["January","february","March","April","May","June","July","August","September","October","November","December"];
  const days=["Sunday","Monday","Tuersday","WednesDay","Thursday","Friday","Saturday"]
 
- var current_month_name=months[date.getMonth()];
+ 
  var currentdayindex=date.getDay();
+ var currentdayname=days[date.getDay()];
  var currentdate=date.getDate();
  var current_monthindex=date.getMonth();
+ var current_month_name=months[date.getMonth()];
  var current_year=date.getFullYear();
- let firstdaycurrentmonthindex=new Date(current_year,current_monthindex,1).getDay();
- 
+ let firstday_index__of_cuttent_month=new Date(current_year,current_monthindex,1).getDay();
  let lastdaycurrentmonth=new Date(current_year,current_monthindex+1,0).getDate();
  let premontlastdate=new Date(current_year,current_monthindex,0).getDate();
  
 var dateshedule=document.querySelector(".date");
-console.log(dateshedule)
  var dates=" ";
 
  // pre month
- for(let x=firstdaycurrentmonthindex;x>0;x--){
+ for(let x=firstday_index__of_cuttent_month;x>0;x--){
     dates+= `<div class="prev_date">${premontlastdate-x+1}</div>` ;
 }
 //current month
@@ -36,7 +36,7 @@ for (let index =1; index <=lastdaycurrentmonth; index++) {
     dates+=`<div class="next_date">${index}</div>`;
  }
  dateshedule.innerHTML=dates;
-console.log(dateshedule);
  dates="";
 
+document.querySelector(".current_month h2").innerHTML=current_month_name;
  
